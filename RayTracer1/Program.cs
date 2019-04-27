2e254570cfbcb483978216cea8d1a70f761c2182
timmcgilly <timmcgilly@gmail.com>
2019-04-27 16:55:46 +0100

    Steps 3 and 4

--- RayTracer1/Program.cs
+++ RayTracer1/Program.cs
@@ -11,11 +11,25 @@ namespace RayTracer1
     {
         static void Main(string[] args)
         {
-            Sphere sphere = new Sphere(new Vector3(-3, 0, -16), 2);
-            Render(sphere);
+            
+            Material ivory =  new Material(new Vector3(0.4f, 0.4f, 0.3f));
+            Material red_rubber = new Material(new Vector3(0.3f, 0.1f, 0.1f));
+
+            List<Sphere> spheres = new List<Sphere>();
+            spheres.Add(new Sphere(new Vector3(-3f, 0f, -16f), 2, ivory));
+            spheres.Add(new Sphere(new Vector3(-1.0f, -1.5f, -12f), 2, red_rubber));
+            spheres.Add(new Sphere(new Vector3(1.5f, -0.5f, -18f), 3, red_rubber));
+            spheres.Add(new Sphere(new Vector3(7f, 5f, -18f), 4, ivory));
+
+            List<Light> lights = new List<Light>();
+            lights.Add(new Light(new Vector3(-20f, 20f, 20f), 1.5f));
+
+
+
+            Render(spheres.ToArray(), lights.ToArray());
         }
 
-        static void Render(Sphere sphere)
+        static void Render(Sphere[] spheres, Light[] lights)
         {
             const int width = 1024;
             const int height = 768;
@@ -32,7 +46,7 @@ namespace RayTracer1
                     float y = (float)(-(2 * (j + 0.5) / (float)height - 1) * Math.Tan(fov / 2));
                     //Console.WriteLine($"x={x} y={y}");
                     Vector3 dir = Vector3.Normalize(new Vector3(x, y, -1)); 
-                    frameBuffer[i + j * width] = CastRay(new Vector3(0, 0, 0), dir, sphere);
+                    frameBuffer[i + j * width] = CastRay(new Vector3(0, 0, 0), dir, spheres, lights);
                 }
             }
 
@@ -52,26 +66,69 @@ namespace RayTracer1
 
         }
 
-        static Vector3 CastRay(Vector3 orig, Vector3 dir, Sphere sphere)
+        static bool SceneIntersect(Vector3 orig, Vector3 dir, Sphere[] spheres, ref Vector3 hit, ref Vector3 normal, ref Material material )
         {
-            float sphere_dist = float.MaxValue;
-            if (!sphere.RayIntersect(orig,dir,ref sphere_dist))
+            float spheres_dist = float.MaxValue;
+            foreach (Sphere sphere in spheres)
+            {
+                float dist_i = 0;
+                if (sphere.RayIntersect(orig, dir, ref dist_i) && dist_i < spheres_dist)
+                {
+                    spheres_dist = dist_i;
+                    hit = orig + (dir * dist_i);
+                    normal = Vector3.Normalize(Vector3.Subtract(hit, sphere.center));
+                    material = sphere.material;
+                }
+            }
+            return spheres_dist < 1000;
+        }
+
+
+        static Vector3 CastRay(Vector3 orig, Vector3 dir, Sphere[] spheres, Light[] lights)
+        {
+            Vector3 point = new Vector3();
+            Vector3 normal = new Vector3();
+            Material material = new Material();
+
+            if (!SceneIntersect(orig,dir,spheres, ref point, ref normal, ref material))
             {
                 return new Vector3(0.2f, 0.7f, 0.8f);
             }
-            return new Vector3(0.4f, 0.4f, 0.3f);
+
+            float diffuse_light_intensity = 0;
+            foreach (Light light in lights)
+            {
+                Vector3 light_dir = Vector3.Normalize(light.position - point);
+                diffuse_light_intensity += light.intensity * Math.Max(0.0f, Vector3.Dot(light_dir, normal));
+            }
+
+            return material.diffuse_color * diffuse_light_intensity;
         }
+    }
+
+    public class Light
+    {
+        public Vector3 position;
+        public float intensity;
+        public Light(Vector3 p, float i)
+        {
+            this.position = p;
+            this.intensity = i;
+        }
+
     }
 
     public class Sphere
     {
         public Vector3 center = new Vector3();
         public float radius;
+        public Material material;
 
-        public Sphere(Vector3 c, float r)
+        public Sphere(Vector3 c, float r, Material m)
         {
             this.center = c;
             this.radius = r;
+            this.material = m;
         }
 
         //Origin is origin of ray. Dir is the direction of the ray
@@ -103,5 +160,19 @@ namespace RayTracer1
             return true;
 
         }
+    }
+
+    public class Material
+    {
+        public Material(Vector3 color)
+        {
+            this.diffuse_color = (color);
+        }
+        public Material()
+        {
+            this.diffuse_color = new Vector3();
+        }
+
+        public Vector3 diffuse_color = new Vector3();
     }
 }
